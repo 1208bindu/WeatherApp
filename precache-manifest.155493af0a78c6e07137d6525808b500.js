a9843b6c7a97916c60e97b1d74028e8ed192f620
Bindu Trikha <bindu.trikha@gmail.com>
2021-01-18 02:18:39 +0530

    Updates

---
+++ precache-manifest.155493af0a78c6e07137d6525808b500.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea300f066edf851e685ffd6a36477e0e",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "0a2942feccac7199865a",
+    "url": "/WeatherApp/static/css/main.0acc4b79.chunk.css"
+  },
+  {
+    "revision": "a0682da35585a96fe625",
+    "url": "/WeatherApp/static/js/2.c38a747f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherApp/static/js/2.c38a747f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2942feccac7199865a",
+    "url": "/WeatherApp/static/js/main.40da952d.chunk.js"
+  },
+  {
+    "revision": "35dda461ec21b70bd0c9",
+    "url": "/WeatherApp/static/js/runtime-main.07269438.js"
+  },
+  {
+    "revision": "2e1e4bbd5726e278cf368e6ac6dca3bf",
+    "url": "/WeatherApp/static/media/spring-mobile.2e1e4bbd.jpg"
+  },
+  {
+    "revision": "a6b582f1d271a7f3f992d44a12f5086c",
+    "url": "/WeatherApp/static/media/spring.a6b582f1.jpg"
+  },
+  {
+    "revision": "13328dc2bbe290f3644e2bb82f1a4d9b",
+    "url": "/WeatherApp/static/media/warm-mobile.13328dc2.jpg"
+  },
+  {
+    "revision": "9d46954b33a5c3356ca04072e7dbc141",
+    "url": "/WeatherApp/static/media/warm.9d46954b.jpg"
+  },
+  {
+    "revision": "83af79288ec2347e072def7f536be4d2",
+    "url": "/WeatherApp/static/media/winter-mobile.83af7928.jpg"
+  },
+  {
+    "revision": "17fe639096ed546087fdecf5156f371c",
+    "url": "/WeatherApp/static/media/winter.17fe6390.jpg"
+  }
+]);
